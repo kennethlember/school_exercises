14a6f78c41de6ce699c0187f203a73a59d6ce1c7
kennethlember <55434323+kennethlember@users.noreply.github.com>
2019-09-17 11:41:23 +0300

    Add files via upload

---
+++ Ülesanded/yl3.py
@@ -0,0 +1,6 @@
+a = input("Sisesta esimene arv: ")
+b = input("Sisesta teine arv: ")
+if int(a) < int(b)
+    print("Nende arvude miinimum on " + a)
+else:
+    print("Nende arvude miinimum on " + b)
