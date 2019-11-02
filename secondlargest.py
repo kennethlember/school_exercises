ad814e7339674d9bbbd6508b86fb1a94ba61ba98
kennethlember <55434323+kennethlember@users.noreply.github.com>
2019-11-02 23:21:57 +0200

    Add files via upload

--- secondlargest.py
+++ secondlargest.py
@@ -1,5 +1,10 @@
-numbers = [4, 10, 8, 21, 25]
-
+numbers = [4, 10, 8, 21, 25, 25, 26]
 numbers.sort()
+repeat = numbers.count(numbers[-1])
 
-print(numbers[-2])
+while repeat != 1:
+    numbers.remove(numbers[-1])
+    repeat = repeat - 1
+    
+if repeat == 1:
+    print(numbers[-2])
